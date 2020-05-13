8653dba36ad1afb53b4af7f557b37b3de38dce1a
Thomas Mazéas <t7mazeas@enib.fr>
2020-05-13 17:19:47 +0200

    Début Implémentation HTML et JS ajout entreprise

--- static/js/addexp.js
+++ static/js/addexp.js
@@ -14,16 +14,43 @@ function Exp(){
     }
 }
 
+
 var vm = new Vue({
     el: "#app", 
     delimiters: ['[[', ']]'],
     data: {
-        user: new User(),
+        user: new Exp(),
+        tempoUser: new Object(),
         confirmPassword: "",
         err: []
     }, 
 
     methods : {
+        openModalEntreprise: function () {
+            this.err = []
+            this.tempoUser = this.exp
+            $('#modifModalEntreprise').modal({
+                show: true,
+                backdrop: false,
+            })
+            console.log('Test')
+        
+        },
+        closeModalEntreprise: function () {
+            this.err = []
+            $('#modifModalEntreprise').modal('hide')
+        },
+        save: function () {
+            this.err = []
+            this.validate()
+            if (this.err.length == 0){
+                this.exp = this.tempoUser
+                console.log("Perf")
+                this.closeModal()
+            } else {
+                console.log("Problem")
+            }
+        },
         validate: function(){
             //Regular expression of an email
             let regMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
