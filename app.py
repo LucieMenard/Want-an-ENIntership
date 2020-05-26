727e56e4d0c396552cad13b69f3870011b50e7da
Lucie <lulu.menard@gmail.com>
2020-05-26 20:16:26 +0200

    Finalisation de Add Expérience
    - Mise en place des affichages des erreurs dans la page addExp.html.
    - Debuggage de l'enregistrement de saveExp()
    - Récupération de l'ID de l'utilisateur qui crée l'expérience
    - Modification de la gestion des entreprises (select à la place de
      input) dans addExp.html
    - Généralisation à tout les autres controles sur liste (durée, type)
    - Récupération de l'ID de l'entreprise
    - Enregistrement des informations contact dans la BDD et récupération de
      l'ID
    - Correction des libelés non associés à leur contrôle

--- app.py
+++ app.py
@@ -2,17 +2,79 @@ import psycopg2
 import json
 import datetime
 from flask import Flask, jsonify, render_template, request, Response, redirect, url_for, session
+from pprint import pprint
 
 app = Flask(__name__)
 app.secret_key='enib'
 
+##############################################################################
+# Tables de référence
+#
+# Par la suite, ces données pourraient être stockées dans la base de données,
+# Pour le moment on simplifie en les mettant içi
+##############################################################################
+
+def getExperienceTypes():
+    return {
+        "cdi": "CDI",
+        "cdd": "CDD",
+        "stage": "Stage",
+        "alt": "Alternance"
+    }
+
+def getExperienceDurations():
+    return {
+        "1m": "1 mois",
+        "2m": "2 mois",
+        "3m": "3 mois",
+        "4m": "4 mois",
+        "5m": "5 mois",
+        "6m": "6 mois",
+        "7m": "7 mois",
+        "8m": "8 mois",
+        "9m": "9 mois",
+        "10m": "10 mois",
+        "11m": "11 mois",
+        "12m": "1 an",
+        "more": "+",  
+    }
+
+def getExperienceDomains():
+    return [
+        {"value": "computing"    , "label": "Informatique"  },
+        {"value": "mecatronic"   , "label": "Mécatronique"  },
+        {"value": "electronic"   , "label": "Electronique"  }
+    ]
+
+def getAllCompanies():
+    # Récupérer la connexion à la base de données
+    # Récupérer tous les enregistrements de la table Entreprise dans la BDD. on veut les champs "id_entreprise" et "name"
+    
+    # Les mettre sous la forme attendue
+    
+    # On essaie de se connecter à la base et on retourne une erreur "503 - service non disponible" si ça échoue
+    try:
+        con = connexionDB()
+    except Exception :
+        return "Impossible de se connecter à la base de données", 503 # http status 503 = "Service unavailable"
+    
+    cur = con.cursor()
+    cur.execute(""" SELECT id_entreprise,name,country FROM "Entreprise" """)
+    data = cur.fetchall()
+    
+    return data
+
+
 def connexionDB():
-    con = psycopg2.connect(database='WAE_Local',
-                           user='postgres',
-                           host='localhost',
-                           password='basket',
+
+    ## BDD de test 
+    con = psycopg2.connect(database='bjiw069frhijwtxapwih',
+                           user='uo3wdoc8qcwuds1kkfoq',
+                           host='bjiw069frhijwtxapwih-postgresql.services.clever-cloud.com',
+                           password='SvJ4jdq7w3n7dMHiO2t6',
                            port='5432')
-                           
+
+    ## BDD WAE        
     # con = psycopg2.connect(database='bn1io6th4a3umkgpylvg',
     #                        user='u4kq3mqz3af6qaixesbk',
     #                        host='bn1io6th4a3umkgpylvg-postgresql.services.clever-cloud.com',
@@ -37,11 +99,16 @@ def recherche():
 
 @app.route('/addexp')
 def addexp():
-    if 'user' in session : 
-        return render_template('addexp.html')
-    else :
+    if 'user' not in session : 
         return redirect(url_for('signin'))
-
+     
+    return render_template(
+        'addexp.html', 
+        types=getExperienceTypes(),
+        domains=getExperienceDomains(),
+        durations=getExperienceDurations(),
+        companies=getAllCompanies()
+    )
 
 @app.route('/contact')
 def contact():
@@ -82,7 +149,6 @@ def testConn():
 #    Requêtes HTTP     #
 ########################
 
-
 #----- Utilisateur -----#
 
 @app.route('/getUser', methods=['POST'])
@@ -121,7 +187,7 @@ def saveUser():
     x = {
         'id': a
     }
-    session['user']=x['id']
+    session['user']=x['id'] # x['id'] = a :)
     con.close()
     return x
 
@@ -151,7 +217,7 @@ def tryPassword():
         return 'False'
     
 
-
+#----- Experiences -----#
 #----- Add Exp -----#
 @app.route('/getExp', methods=['POST'])
 # Récupère une expérience à partir de son ID
@@ -163,21 +229,94 @@ def getExp():
     data = fetchToJson(cur.fetchall())
     return Response(json.dumps(data[0]))
 
-
-#----- Experiences -----#
-
 @app.route('/saveExp', methods=['POST'])
 # Sauvegarde une expérience et renvoie son ID récemment crée.
-# ! Faire le renvoie id
 def saveExp():
+    # Vérification de la session
+    if 'user' not in session : 
+        return "Accès interdit !", 401 # HTTP status 401 = "Authentification necessaire"
+        
+    # Connexion à la base de données
+    try :
+        con = connexionDB()
+    except Exception as e :
+        #Renvoie d'une erreur 503 si la tentative de connexion
+        return "Impossible de se connecter à la BDD", 503 # HTTP status 503 = "Service unavailable"
+
+    # Recupération des données de contact saisies dans le formulaire
+    contact = json.loads(request.form['newContact'])
+
+    # Recupération des données d'expérience saisies dans le formulaire
     exp = json.loads(request.form['newExp'])
+
+    # Valide les données transmises, génère une erreur si tout n'est pas ok (-> TODO)
+    # if !validate(exp):
+    #    return "données incorrectes", 400
+
+    #Enregistrement des données du contact dans la table Contact de la BDD
+    try :
+        #Création du curseur pour faire les requetes SQL
+        cur = con.cursor()
+        cur.execute("""INSERT INTO "Contact"("last_Name","first_Name","phone_number", "mail_Contact", "enibien") VALUES (%s, %s, %s, %s, %s); """,
+                                                 (contact['Nom'], contact['Prenom'], contact['Telephone'], contact['Email'], contact['Enibien']))
+        con.commit()
+    except Exception as e :
+        return "Impossible d'enregistrer le contact : \n" + str(e), 503 # TODO : trouver le code erreur HTTP qui convient à la place du 503
+
+    #Récupération de l'ID du contact lié à l'entreprise
+    cur.execute('SELECT LASTVAL()')
+    idContact = cur.fetchone()[0]
+
+    #Récupération de l'ID de l'utilisateur
+    ident = session['user']
+
+    #Enregistrement des données dans la table Experience de la BDD 
+    try :
+        #Création du curseur pour faire les requetes SQL
+        cur = con.cursor()
+        cur.execute("""INSERT INTO "Experience"("ident","env_grade","type", "domain", "start_date", "end_date", "money", "feel_grade", "duration", "contact", "company", "description") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s); """,
+                                                 (ident,1,exp['Type'], exp['Domain'], exp['StartDate'],exp['EndDate'], exp['Money'], exp['FeelGrade'], exp['Duration'], idContact, exp['Company'], exp['Description']))
+        con.commit()
+    except Exception as e :
+        return "Impossible d'enregistrer l'expérience : \n" + str(e), 503 # TODO : trouver le code erreur HTTP qui convient à la place du 503
+
+    #Récupération de l'ID
+    cur.execute('SELECT LASTVAL()')
+    id = cur.fetchone()[0]
+
+    #Retourne une réponse JSON 200 (=OK) contenant l'ID
+    return {'id':id}
+
+#-- Contact --#
+@app.route('/getContact', methods=['POST'])
+# Récupère un contact à partir de son ID
+def getContact():
     con = connexionDB()
     cur = con.cursor()
-    idContact = getContact()
-    cur.execute("""INSERT INTO "Experience"("type", "domain", "start_date", "end_date", "money", "feel_grade", "duration", "contact", "company", "description") VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s); """,
-                (exp['Type'], exp['Domain'], exp['StartDate'], exp['EndDate'], exp['Money'], exp['FeelGrade'], exp['Duration'], idContact, exp['Company'], exp['Description']))
-    con.commit()
+    id = request.form['newExp']
+    cur.execute(""" SELECT * FROM "Contact" WHERE "Contact".ident = %s""", (id,))
+    data = fetchToJson(cur.fetchall())
+    con.close()
+    return Response(json.dumps(data[0]))
 
+
+@app.route('/saveContact', methods=['POST'])
+# Sauvegarde un contact
+def saveContact():
+    contact = json.loads(request.form['newContact'])
+    pprint(contact)
+    con = connexionDB()
+    cur = con.cursor()
+    cur.execute("""INSERT INTO "Contact"("last_Name", "first_Name", "phone_number", "mail_Contact", "enibien") VALUES (%s, %s, %s, %s, %s) RETURNING "ident"; """,
+                (contact['Nom'], contact['Prenom'], contact['Telephone'], contact['Email'], contact['Enibien']))
+    a = cur.fetchone()[0]
+    con.commit()
+    x = {
+       'id': a
+    }
+    return x   
+
+#-- Entreprise --#
 @app.route('/saveEntreprise', methods=['POST'])
 def saveEntreprise():
     entreprise = json.loads(request.form['newEntreprise'])
@@ -191,35 +330,12 @@ def saveEntreprise():
                 (entreprise['Name'],entreprise['Address'],entreprise['Postal_Code'],entreprise['City'],entreprise['Country'],0))
     a = cur.fetchone()[0]
     con.commit()
-    x = {
-       'id': a
+    return {
+       'id': a,
+       'name': entreprise['Name'],
+       'country': entreprise['Country']
     }
-    return x
     
-
-#-- Contact --#
-@app.route('/getContact', methods=['POST'])
-# Récupère un contact à partir de son ID
-def getContact():
-    con = connexionDB()
-    cur = con.cursor()
-    id = request.form['id']
-    cur.execute(""" SELECT * FROM "Contact" WHERE "Contact".ident = %s""", (id,))
-    data = fetchToJson(cur.fetchall())
-    con.close()
-    return Response(json.dumps(data[0]))
-
-
-# @app.route('/saveContact', methods=['POST'])
-# # Sauvegarde un contact
-# # ! Faire le renvoie id pour le réutiliser avec l'expérience
-# def saveContact():
-#     contact = json.loads(request.form['newContact'])
-#     con = connexionDB()
-#     cur = con.cursor()
-#     cur.execute("""INSERT INTO "Contact"("last_Name", "first_Name", "phone_number", "mail_Contact", "enibien") VALUES (%s, %s, %s, %s, %s); """, (contact['Nom'], contact['Prenom'], contact['Telephone'], contact['Email'], contact['Enibien']))
-#     con.commit()
-
 
 #----- Connexion -----#
 
--- static/js/addexp.js
+++ static/js/addexp.js
@@ -1,21 +1,13 @@
 function Exp() {
     this.Type = ""
     this.Domain = ""
-    this.Company = ""
-    this.Money = ""
+    this.Company = 0
+    this.Money = false
     this.Duration = ""
-    this.StartDate = ""
-    this.EndDate = ""
+    this.StartDate = "2020-05-25"
+    this.EndDate = "2020-06-25"
     this.FeelGrade = 0
     this.Description = ""
-    this.EndDate = ""
-    this.Contact = function Contact() {
-        this.Nom = ""
-        this.Prenom = ""
-        this.Email = ""
-        this.Telephone = ""
-        this.Enibien = False
-    }
 }
 function Entreprise() {
     this.Name = ""
@@ -26,7 +18,13 @@ function Entreprise() {
     this.Country = ""
     this.Grade = 0
 }
-
+function Contact() {
+    this.Nom = ""
+    this.Prenom = ""
+    this.Email = ""
+    this.Telephone = ""
+    this.Enibien = false
+}
 
 var vm = new Vue({
     el: "#app",
@@ -34,6 +32,7 @@ var vm = new Vue({
     data: {
         exp: new Exp(),
         entreprise: new Entreprise(),
+        contact: new Contact(),
         tempoExp: new Object(),
         confirmPassword: "",
         err: []
@@ -41,67 +40,71 @@ var vm = new Vue({
 
     methods: {
         openModalEntreprise: function () {
-            this.err = []
-            this.entreprise = new Entreprise()
+            this.err = [];
+            this.entreprise = new Entreprise();
             $('#modifModalEntreprise').modal({
                 show: true,
                 backdrop: false,
-            })
-            console.log('Test')
+            });
+            console.log('Test');
 
         },
         closeModalEntreprise: function () {
-            this.err = []
-            $('#modifModalEntreprise').modal('hide')
+            this.err = [];
+            $('#modifModalEntreprise').modal('hide');
         },
         save: function () {
-            this.err = []
-            this.validate()
+            this.err = [];
+            this.validate();
             if (this.err.length == 0) {
-                console.log("Perf")
-                this.closeModalEntreprise()
+                console.log("Perf");
+                this.closeModalEntreprise();
             } else {
-                console.log("Problem")
+                console.log("Problem");
             }
         },
         validate: function () {
             //Regular expression of an email
             let regMail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-            let regMdp = /^[\W\w]{6,}$/
             if (!this.exp.Company || !this.exp.Type || !this.exp.Domain || !this.exp.Duration) {
-                this.err.push('Les champs marqués avec une * sont obligatoires')
+                this.err.push('Les champs marqués avec une * sont obligatoires');
             }
-            if (!this.exp.contact.Email.match(regMail)) {
-                this.err.push("L'email n'est pas valide")
+            if (this.contact && this.contact.Email && !this.contact.Email.match(regMail)) {
+                this.err.push("L'email n'est pas valide");
             }
-
         },
         validateEntreprise: function () {
             if (!this.entreprise.Name || !this.entreprise.Address || !this.entreprise.Postal_Code || !this.entreprise.City || !this.entreprise.Country) {
-                this.err.push('Tous les champs sont obligatoires')
+                this.err.push('Tous les champs sont obligatoires');
             }
         },
 
         submit: function () {
-            let vue = this
-            this.err = []
-            this.validate()
+            this.err = [];
+            this.validate();
             if (this.err.length == 0) {
                 $.ajax({
                     url: '/saveExp',
                     data: {
-                        'newExp': JSON.stringify(vue.exp)
+                        'newExp': JSON.stringify(vm.exp),
+                        'newContact': JSON.stringify(vm.contact)
                     },
                     type: 'POST',
-                    error: function (a, status, error) {
-                        console.log('Erreur : ' + error + '\nStatus : ' + status)
+                    success: function (data, status, xhr) {
+                        alert("L'expérience a bien été créée ! Son id :" + data.id);
+                        //TODO redirection sur la page HTML de cette expérience
+                        //TODO supprimer les dates et la description
+                        document.location.href = '/profil/'+a['id'];
+                    },
+                    error: function (xhr, status, error) {
+                        vm.err.push(xhr.responseText);
                     }
                 })
             }
         },
         submitEntreprise: function () {
-            let vue = this
-            this.err = []
+            let vue = this;
+            this.err = [];
             this.validateEntreprise()
             if (this.err.length == 0) {
                 $.ajax({
@@ -112,14 +115,17 @@ var vm = new Vue({
                     type: 'POST',
                     success: function (a) {
                         if (a == 'False') {
-                            vue.err.push("Ce nom d'entreprise existe déjà")
+                            vue.err.push("Ce nom d'entreprise existe déjà");
                         } else {
-                            console.log(a)
-                            vue.closeModalEntreprise()
+                            console.log(a);
+                            let label = a.name + ' (' + a.country + ')';
+                            $('#companies').append(new Option(label, a.id));
+                            vue.exp.Company = a.id; // Le fait de changer la propriété du modèle dans vue.js sélectionne l'option dans le html
+                            vue.closeModalEntreprise();
                         }
                     },
                     error: function (a, status, error) {
-                        console.log('Erreur : ' + error + '\nStatus : ' + status)
+                        console.log('Erreur : ' + error + '\nStatus : ' + status);
                     }
                 })
             }
