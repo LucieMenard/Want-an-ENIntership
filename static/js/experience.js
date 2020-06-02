42b02643bddeb87bac199e21ca9820be84b0b5e5
Lucie <lulu.menard@gmail.com>
2020-06-02 18:41:50 +0200

    [Full] affichage des experiences

--- static/js/experience.js
+++ static/js/experience.js
@@ -8,22 +8,16 @@ function Exp(domaine,duree,remuneration,type,identite,idCompany,grade,idContact)
     this.IdContact = idContact
     this.Grade = grade
 }
-function Company(id, nom, adresse, cp, ville, pays, note) {
-    this.Id = id
+function Company(nom) {
     this.Nom = nom
-    this.Adresse = adresse
-    this.CodePostal = cp
-    this.Ville = ville
-    this.Pays = pays
-    this.Note = note
 }
-function Contact(id, nom, prenom, telephone, mail, enibien){
-    this.Id = id
+function Contact(nom, prenom){
     this.Nom = nom
     this.Prenom = prenom
-    this.Telephone = telephone
-    this.Mail = mail
-    this.Enibien = enibien
+}
+function User(nom,prenom){
+    this.Nom = nom
+    this.Prenom = prenom
 }
 
 var vm = new Vue({
@@ -32,7 +26,8 @@ var vm = new Vue({
     data: {
         entreprise: new Object(),
         experience: new Object(),
-        contact: []
+        contact: new Object(),
+        user: new Object()
     },
     methods: {
         getExperience: function(id){
@@ -52,6 +47,7 @@ var vm = new Vue({
                                              d['idContact'],
                                              d['grade'])
                     console.log(vue.entreprise)
+                    var idUser=d['idCompany']
                 },
                 error: function(xhr, status, error){
                     console.log('Erreur : ' + error + '\nStatus : ' + status)
@@ -69,7 +65,7 @@ var vm = new Vue({
                 type: 'POST',
                 success: function(d, status, xhr){
                     console.log(d)
-                    vue.entreprise = new Company(d['id'], d['nom'], d['adresse'], d['cp'], d['ville'], d['pays'], d['note'])
+                    vue.entreprise = new Company(d['nom'])
                     console.log(vue.entreprise)
                 },
                 error: function(xhr, status, error){
@@ -77,21 +73,32 @@ var vm = new Vue({
                 }
             })
         },
-
-        getContacts: function(){
+        getContact: function(id){
             let vue = this
             $.ajax({
-                url: '/getContactFromComp/'+vue.entreprise.Id,
+                url: '/getOneContact/'+id,
                 type: 'GET',
                 success: function(d, status, xhr){
-                    contacts = JSON.parse(d)
-                    contacts.forEach(c => {
-                        contact = new Contact(c['id'], c['nom'], c['prenom'], c['phone'], c['mail'], c['enibien'])
-                        vue.contacts.push(contact)
-                    });
+                    console.log(d)
+                    vue.contact = new Contact(d['name'], d['surname'])
+                    console.log(vue.contact)
                 },
                 error: function(xhr, status, error){
-                    console.log(xhr)
+                    console.log('Erreur : ' + error + '\nStatus : ' + status)
+                }
+            })
+        },
+        getUser: function(id){
+            let vue = this
+            $.ajax({
+                url: '/getOneUser/'+id,
+                type: 'GET',
+                success: function(d, status, xhr){
+                    console.log(d)
+                    vue.user = new User(d['name'], d['surname'])
+                    console.log(vue.user)
+                },
+                error: function(xhr, status, error){
                     console.log('Erreur : ' + error + '\nStatus : ' + status)
                 }
             })
@@ -102,6 +109,10 @@ var vm = new Vue({
         var id = link[link.length - 1]
         this.getExperience(id)
         this.getCompany(id)
-        // this.getContacts()
+        this.getContact(id)
+        
+        console.log(idUser)
+        // idUser=this.getIDUser()
+        this.getUser(idUser)
     }
 })
